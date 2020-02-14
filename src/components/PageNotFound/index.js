b46d90949cc6b3210dd1e2665263a0ac4e73d3b3
Tyler Marenger <tjmareng@mtu.edu>
2020-02-14 18:21:33 -0500

    Created React App

---
+++ src/components/PageNotFound/index.js
@@ -0,0 +1,7 @@
+  
+import React from 'react'
+import { Image } from 'semantic-ui-react'
+
+const ImageExampleImage = () => <Image src='/images/wireframe/image.png' size='small' />
+
+export default ImageExampleImage
