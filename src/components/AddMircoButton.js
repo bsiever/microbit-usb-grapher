86c548921460d834263aeabeab8307fc5527ab72
Ryan Philipps <raphilip@mtu.edu>
2020-03-17 15:18:01 -0400

    Created an Add Microbit button

---
+++ src/components/AddMircoButton.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import {Button} from 'semantic-ui-react';
+import {uBitConnectDevice} from '../utils/microbit-api'
+
+/**
+ * 
+ * @param {*} props
+ *  onAddComplete: callback function when the mirco is added or failed to add
+ *   The call back should take at least three parameters
+ *     1. type: This is the type of message sent from the micro
+ *     2. device: This is an id for the device
+ *     3. data: Whatever data the micro sends 
+ */
+export function AddMircoButton(props) {
+
+    let connectMicro = () => {
+        uBitConnectDevice(props.onAddComplete)
+    }
+
+    return <Button onClick={e => connectMicro()}>Add Micro</Button>
+}
